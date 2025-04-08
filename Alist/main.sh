200572b656714d116a1b84a9e5dd245b21487ba0
oooo <149586090+rootoooo@users.noreply.github.com>
2025-04-08 16:19:48 +0800

    Delete Alist directory

--- Alist/main.sh
+++
@@ -1,4 +0,0 @@
-echo "Alist run with replit"
-URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
-while true; do curl -s "https://$URL" > /dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
-chmod +x ast && ./ast server
